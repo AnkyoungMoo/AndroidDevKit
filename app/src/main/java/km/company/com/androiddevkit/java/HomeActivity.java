70059f9b1d271662c9bddf59d664d87007390080
KimHunJin <ysg01129@naver.com>
2019-02-24 16:14:19 +0900

    [Init] 자바 layout 연결

--- app/src/main/java/km/company/com/androiddevkit/java/HomeActivity.java
+++ app/src/main/java/km/company/com/androiddevkit/java/HomeActivity.java
@@ -1,16 +1,17 @@
 package km.company.com.androiddevkit.java;
 
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 
+import km.company.com.androiddevkit.R;
 import km.company.com.androiddevkit.java.base.BaseActivity;
 import km.company.com.androiddevkit.java.utils.Log;
 
 public class HomeActivity extends BaseActivity {
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
 
         Log.log("java");
 
